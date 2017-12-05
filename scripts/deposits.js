cd030699494f38aa0047233e7dd1f5f23bff3b14
Everton Fraga <ev@ethereum.org>
2017-12-05 10:56:18 -0500

    Adding internal txs

--- scripts/deposits.js
+++ scripts/deposits.js
@@ -4,10 +4,11 @@ const got = require('got');
 const pcdAddress = '0xa4E23286C06208645f52c57A6eF5a510e87A6d76'.toLowerCase();
 const apiKey = process.env.ETHERSCAN_API_KEY;
 const prspPublishedBlock = 3965050;
-const startBlock = 	4330496 + 1;
-const endBlock = 4473116 + 1;
+const startBlock = 	4473116 + 1;
+const endBlock = 4657930 + 1;
 
-const apiEndpoint = (pcdAddress, startBlock, endBlock, apiKey) => `http://api.etherscan.io/api?module=account&action=txlist&address=${pcdAddress}&startblock=${startBlock}&endblock=${endBlock}&sort=desc&apikey=${apiKey}`;
+const txListEndpoint = (pcdAddress, startBlock, endBlock, apiKey) => `http://api.etherscan.io/api?module=account&action=txlist&address=${pcdAddress}&startblock=${startBlock}&endblock=${endBlock}&sort=desc&apikey=${apiKey}`;
+const internalTxListEndpoint = (pcdAddress, startBlock, endBlock, apiKey) => `http://api.etherscan.io/api?module=account&action=txlistinternal&address=${pcdAddress}&startblock=${startBlock}&endblock=${endBlock}&sort=desc&apikey=${apiKey}`;
 
 var balances = [];
 
@@ -18,13 +19,20 @@ const accountForBalance = (account, value) => {
 
 const incomingOnly = tx => tx.from != pcdAddress;
 
-got(apiEndpoint(pcdAddress, startBlock, endBlock, apiKey), {json: true}).then(response => {
+
+got(txListEndpoint(pcdAddress, startBlock, endBlock, apiKey), {json: true}).then(response => {
   response.body.result.filter(incomingOnly).map(tx => {
     accountForBalance(tx.from, tx.value);
   });
 
-  Object.keys(balances).map(k => {
-    console.log(`${k}\t${balances[k]}`);
+  got(internalTxListEndpoint(pcdAddress, startBlock, endBlock, apiKey), {json: true}).then(internalResponse => {
+    internalResponse.body.result.filter(incomingOnly).map(tx => {
+      accountForBalance(tx.from, tx.value);
+    });
+
+    Object.keys(balances).map(k => {
+      console.log(`${k}\t${balances[k]}`);
+    });
   });
 })
 .catch(error => {
